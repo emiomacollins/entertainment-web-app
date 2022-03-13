a640b2e4eaeb210d35e562b2f3d1a9046449f0f1
Emioma collins <emiomacollins96@gmail.com>
2022-03-12 21:11:49 -0500

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import styledComponents from 'styled-components';
+import { routes } from './constants/routes';
+import Login from './pages/login';
+import SignUp from './pages/signup';
 
 function App() {
-	return <div>App</div>;
+	return (
+		<Routes>
+			<Route path={routes.login} element={<Login />} />
+			<Route path={routes.signUp} element={<SignUp />} />
+			<Route path="*" element={<Layout></Layout>} />
+		</Routes>
+	);
 }
 
 export default App;
+
+const Layout = styledComponents.div`
+    
+`;
