bf03bcb40ded456b839cc7522983b39ccaaba588
Emioma collins <emiomacollins96@gmail.com>
2022-03-13 20:17:06 -0400

    auth logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { tablet } from './constants/mediaQueries';
 import { routes } from './constants/routes';
+import { auth } from './firebase/init';
 import Login from './pages/auth/login';
 import SignUp from './pages/auth/signup';
-import { getUser } from './redux/user/userSlice';
+import { setUser } from './redux/user/userSlice';
 
 function App() {
-	const user = useSelector(getUser);
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [authInitialized, setAuthInitialized] = useState(false);
 
 	useEffect(() => {
-		if (!user) navigate(routes.login);
-	}, [navigate, user]);
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// add serializable properties only
+				const { uid, providerData: otherProps } = user;
+				dispatch(setUser({ uid, ...otherProps[0] }));
+			} else {
+				dispatch(setUser(null));
+				navigate(routes.login);
+			}
+			setAuthInitialized(true);
+		});
+	}, []);
 
-	return (
+	return authInitialized ? (
 		<Routes>
 			<Route path={routes.login} element={<Login />} />
 			<Route path={routes.signUp} element={<SignUp />} />
-			<Route path="*" element={<Layout></Layout>} />
+			<Route path='*' element={<Layout>Layout</Layout>} />
 		</Routes>
-	);
+	) : null;
 }
 
 export default App;
 
-const Layout = styled.div``;
+const Layout = styled.div`
+	display: grid;
+
+	@media (min-width: ${tablet}) {
+		grid-template-columns: auto 1fr;
+	}
+`;
