3142ffb1fe6199b12659a9fc527334a6d58c3f68
Emioma collins <emiomacollins96@gmail.com>
2022-03-14 19:36:36 -0400

    refactor reusable components

--- src/components/styled-components/Button.jsx
+++ src/components/styled-components/Button.jsx
@@ -5,7 +5,6 @@ export const Button = styled.button`
 	border: 0;
 	border-radius: var(--radius-300);
 	background: var(--red);
-	color: var(--light);
 	transition: 0.2s;
 
 	&:hover {
--- src/components/styled-components/form-elements/Textbox.jsx
+++ src/components/styled-components/form-elements/Textbox.jsx
@@ -3,12 +3,10 @@ import styled from 'styled-components';
 export const Textbox = styled.input`
 	padding: 1.8rem 1rem;
 	background: transparent;
-	color: var(--light);
 	border: 0;
 	border-bottom: 1.5px solid var(--blue);
 	outline: 0;
 	transition: border-color 0.3s;
-	caret-color: var(--red);
 
 	&::placeholder {
 		opacity: 0.5;
