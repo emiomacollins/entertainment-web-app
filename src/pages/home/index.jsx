7fb3dd793ade7942ccdca9bcb61fe5d770e2adc9
Emioma collins <emiomacollins96@gmail.com>
2022-03-14 22:43:40 -0400

    home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
-	return <div>Home</div>;
+	return <Container>Home</Container>;
 }
 
 export default Home;
+
+const Container = styled.div``;
