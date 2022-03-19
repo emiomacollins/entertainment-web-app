7fcb5d807eaf711a9663469644b37f1d702ae21e
Emioma collins <emiomacollins96@gmail.com>
2022-03-19 13:46:27 -0400

    react query setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'normalize-css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -8,12 +9,16 @@ import App from './App';
 import './css/global.css';
 import { persistor, store } from './redux/store';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Provider store={store}>
 				<PersistGate persistor={persistor}>
-					<App />
+					<QueryClientProvider client={queryClient}>
+						<App />
+					</QueryClientProvider>
 				</PersistGate>
 			</Provider>
 		</BrowserRouter>
