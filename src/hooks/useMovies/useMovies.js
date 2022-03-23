dd3055de64c807230022952ff5af8e9a74f5d61f
Emioma collins <emiomacollins96@gmail.com>
2022-03-23 19:28:54 -0400

    modified default options

--- src/hooks/useMovies/useMovies.js
+++ src/hooks/useMovies/useMovies.js
@@ -2,8 +2,5 @@ import { useQuery } from 'react-query';
 import { getMovies } from './api';
 
 export function useMovies() {
-	return useQuery('getMovies', getMovies, {
-		refetchOnWindowFocus: false,
-		refetchOnMount: false,
-	});
+	return useQuery('getMovies', getMovies, { refetchOnMount: false });
 }
